94dd3e92b789d8da3a033fd6050dcf79cbe4ea06
mystica2000 <mysticainf@gmail.com>
2022-09-03 01:47:52 +0530

    Added Plugins

---
+++ src/js/main.js
@@ -0,0 +1,45 @@
+mediumZoom('.post-img',{
+    margin: 10,
+    background: '#FFF',
+    scrollOffset: 0,
+})
+
+
+const url = new URL(window.location)
+
+
+var id = ["blog","others","home"]
+
+function firstWord(str) {
+    str = String(str)
+
+    if(str == "/") {
+        str = "/home"
+    }
+
+    let activeLink = [];
+
+    for(let i=1;i<str.length;i++) {
+        if(str[i]=='/')
+        {
+            break;
+        }
+        activeLink.push(str[i]);
+    }
+
+    str = activeLink.join("")
+
+    for(let i=0;i<id.length;i++) {
+        var doc = document.getElementById(id[i])
+        if(id[i] === str) {
+            doc.classList.add("active")
+        } else {
+            if(doc.classList.contains("active")) {
+              doc.classList.remove("active")
+            }
+        }
+    }
+}
+
+
+firstWord(url.pathname)
